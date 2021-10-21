2128e21ad8a664b7ac9d9d2663e9f937c692cf94
George Elliott <George.Elliott@doh.wa.gov>
2021-10-21 13:04:34 -0700

    Escape the Styles in DIV

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -16,11 +16,33 @@ const SplashScreen = () => {
           </Trans>
         </h1>
       </div>
-      <div style="text-align: center;">
+      <div style={{ textAlign: "center"}}>
             To access your online verification records, please select from the options below.
             <br />
             If you have questions about WA Verify, please call 800-525-0127, press #
-      </div>                  
+      </div>
+      <div style={{border: "1px solid black"}}>
+        {/* Left Pane */}
+        <div id="PaneLeft">
+          WA Verify<br />
+          <img src="/imgs/waverifylogo.png" height='71px' width='263px' alt="WaVerify Logo" /><br />
+          If you are looking for your State COVID-19 Verification Record with QR Code
+          <ul>
+            <li>*Available in 40 languages</li>
+            <li>*SMART Health Card compatible</li>
+          </ul>
+          <img src="/imgs/add-to-apple-health.svg"  alt="Add to Apple Health Logo" />
+          <img src="/imgs/smart-logo.svg"  alt="SmartHealth Logo" />
+          <div style="border: 1px solid black; background-color: lightgreen;">
+            <Link to="/Dashboard"  style={{
+            color: "#0d6efd",
+            margin: "0",
+            textDecoration: "underline",
+            }}>Access Here</Link>
+          </div>                
+        </div>  
+
+      </div>                     
     </div>
   );  
 };
