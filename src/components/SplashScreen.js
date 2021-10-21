0b6c75d4f2b07b566c32fec4087be47924cc6f9b
George Elliott <George.Elliott@doh.wa.gov>
2021-10-21 11:48:07 -0700

    SplashScreen content is not appearing.

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -9,16 +9,13 @@ const SplashScreen = () => {
   return (
       <div className="received-screen-container bodyContainer">
         <section>
-        <h1
-            style={{
-              fontSize: "38px",
-              fontWeight: "700",
-              color: "#F06724",
-              margin: "50px 0",
-            }}
-          >
-            Welcome to WA Verify!
-          </h1>          
+          <div>
+            <h1 style={{ color: "#F06724", fontSize: "38px", marginTop: "25px" }}>
+              <Trans i18nKey="SplashScreenpage.title">
+                Welcome to WA Verify!
+              </Trans>
+            </h1>
+          </div>         
             <div style="text-align: center;">
               To access your online verification records, please select from the options below.
               <br />
