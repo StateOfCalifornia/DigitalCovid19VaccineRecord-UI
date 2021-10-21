e623c12b59626cdf6f6cfe17c800da1ca396f240
George Elliott <George.Elliott@doh.wa.gov>
2021-10-21 12:20:05 -0700

    Reduced content to ensure anything is even being deployed.

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -9,21 +9,7 @@ const SplashScreen = () => {
 
   return (
     <div className={"received-screen-container bodyContainer"}>
-      <section>
-        <div>
-          <h1 style={{ color: "#F06724", fontSize: "38px", marginTop: "25px" }}>
-            <Trans i18nKey="SplashScreenpage.title">
-              Welcome to WA Verify!
-            </Trans>
-          </h1>
-        </div>         
-        <div style="text-align: center;">
-          To access your online verification records, please select from the options below.
-          <br />
-          If you have questions about WA Verify, please call 800-525-0127, press #
-        </div>
-
-      </section>
+      Splash Screen
     </div>
   );  
 };
