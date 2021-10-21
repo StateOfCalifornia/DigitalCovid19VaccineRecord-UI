45dd1c88b22cfbdc92210068dfe31a872c6c3404
George Elliott <George.Elliott@doh.wa.gov>
2021-10-21 12:12:43 -0700

    Adding smaller DIV (attempting to get something to appear on the site)

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -17,69 +17,12 @@ const SplashScreen = () => {
             </Trans>
           </h1>
         </div>         
-          <div style="text-align: center;">
-            To access your online verification records, please select from the options below.
-            <br />
-            If you have questions about WA Verify, please call 800-525-0127, press #
-          </div>
-          <div style="border: 1px solid black;">
-            {/* Left Pane */}
-            <div id="PaneLeft">
-              WA Verify<br />
-              <img src="/imgs/waverifylogo.png" height='71px' width='263px' alt="WaVerify Logo" /><br />
-              If you are looking for your State COVID-19 Verification Record with QR Code
-              <ul>
-                <li>*Available in 40 languages</li>
-                <li>*SMART Health Card compatible</li>
-              </ul>
-              <img src="/imgs/add-to-apple-health.svg"  alt="Add to Apple Health Logo" />
-              <img src="/imgs/smart-logo.svg"  alt="SmartHealth Logo" />
-              <div style="border: 1px solid black; background-color: lightgreen;">
-                <Link to="/Dashboard"  style={{
-                color: "#0d6efd",
-                margin: "0",
-                textDecoration: "underline",
-                }}>Access Here</Link>
-              </div>                
-            </div>
+        <div style="text-align: center;">
+          To access your online verification records, please select from the options below.
+          <br />
+          If you have questions about WA Verify, please call 800-525-0127, press #
+        </div>
 
-            {/* Center Pane */}
-            <div id="PaneCenter">
-              MyIR Mobile<br />
-              <img src="/imgs/myirmobillogo.png"  alt="MyIrMobile Logo" /><br />
-              If you are looking for your household’s state COVID-19 Certificate
-              <ul>
-                <li>Only in English</li>
-              </ul>
-              <div style="border: 1px solid black; background-color: lightgreen;">
-                <Link to="/Dashboard"  style={{
-                color: "#0d6efd",
-                margin: "0",
-                textDecoration: "underline",
-                }}>Register Here</Link>
-              </div>                
-            </div>
-
-            {/* Right Pane */}
-            <div id="PaneRight">
-            State School Certificate of Immunizations<br />
-              <img src="/imgs/wastateschoolcertimmunizationslogo.png"  alt="WA State School Certification of Immunizations Logo" /><br />
-              If you are looking for your children’s State School Certificate of Immunizations
-              <ul>
-                <li>Only in English</li>
-              </ul>
-              <img src="/imgs/add-to-apple-health.svg"  alt="Add to Apple Health Logo" />
-              <img src="/imgs/smart-logo.svg"  alt="SmartHealth Logo" />
-            </div>
-            <div style="border: 1px solid black; background-color: lightgreen;">
-            <Link to="/Dashboard"  style={{
-                color: "#0d6efd",
-                margin: "0",
-                textDecoration: "underline",
-              }}>Access Here</Link>
-              </div>                                
-          </div>
-          
       </section>
     </div>
   );  
