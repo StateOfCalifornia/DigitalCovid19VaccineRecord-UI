62adb274da338480e208d0937d85154c818531a4
George Elliott <George.Elliott@doh.wa.gov>
2021-10-25 04:25:36 -0700

    Switch back to Dashboard as Landing page. Replaced FaviIcon image

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ function App() {
       <div className="App">
         <Router>
           <Header />
-          <Route path="/Dashboard" component={Dashboard} />
+          <Route path="/" component={Dashboard} />
           <Route path="/received" component={ReceivedScreen} />
           <Route path="/FAQ" component={FAQScreen} />
-          <Route exact path="/" component={SplashScreen} />          
+          <Route exact path="/SplashScreen" component={SplashScreen} />          
           <Route path="/qr/:lang/:id" component={QrScreen} />
         </Router>
         <Footer />
