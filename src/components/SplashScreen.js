b8c3ac81875618597386cc0096c54acfdf3e2145
George Elliott <George.Elliott@doh.wa.gov>
2021-10-25 17:25:40 -0700

    Allow the emailRegex to test for UPPER and Lower case text

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -21,43 +21,46 @@ const SplashScreen = () => {
         <br />
         If you have questions about WA Verify, please call 800-525-0127, press #
       </div>
-      <div className="container" style={{paddingTop: 30}}>
+      <div className="container" style={{ paddingTop: 30 }}>
         <div className="row">
-          <div id="paneLeft" class="shadow p-3 mb-5 bg-body rounded col-sm-4">
-            <h2 style={{fontSize: "h5"}}>WA Verify</h2>
-            <img src="/imgs/waverifylogo.png" height='71px' width='263px' alt="" />
-            <p style={{ paddingBottom: 30 }}>If you are looking for your State COVID-19 Verification Record with QR Code</p>
-            <ul>
-              <li style={{ listStyleType: "none" }}>°&nbsp;Available in 40 languages</li>
-              <li style={{ listStyleType: "none" }}>°&nbsp;SMART Health Card compatible</li>
-            </ul>
-
-
-            <div style={{ textAlign: "center", paddingTop: 10, paddingBottom: 10 }}>
-              <img src="/imgs/add-to-apple-health.svg" alt="Add to Apple Health Logo" style={{ alignSelf: "baseline", height: "35px", paddingRight: 10 }} />
-              <img src="/imgs/google-pay-black.svg" style={{ alignSelf: "baseline", height: "35px" }} alt="Google Pay Logo" />
-            </div>
+          <div id="paneLeft" class="shadow p-3 mb-5 bg-body rounded col-sm-4" style={{ width: 300, textAlign: "center" }}>
             <div>
-              <a class="vc_general vc_btn3  vc_btn3-shape-rounded  vc_btn3-color-success" style={{ backgroundColor: "#79b974" }} href="/Dashboard" title="Click here to go to the WaVerify Verification Record page">Access Here</a>
+              <h2 style={{ fontSize: "h5", textAlign: "center" }}>WA Verify</h2>
+              <img src="/imgs/waverifylogo.png" height='71px' width='263px' alt="" />
+              <p style={{ paddingBottom: 30 }}>If you are looking for your State COVID-19 Verification Record with QR Code</p>
+              <ul>
+                <li style={{ listStyleType: "none" }}>°&nbsp;Available in 40 languages</li>
+                <li style={{ listStyleType: "none" }}>°&nbsp;SMART Health Card compatible</li>
+              </ul>
+
+
+              <div style={{ textAlign: "center", paddingTop: 10, paddingBottom: 10 }}>
+                <img src="/imgs/add-to-apple-health.svg" alt="Add to Apple Health Logo" style={{ alignSelf: "baseline", height: "35px", paddingRight: 10 }} />
+                <img src="/imgs/google-pay-black.svg" style={{ alignSelf: "baseline", height: "35px" }} alt="Google Pay Logo" />
+              </div>
             </div>
+            <div classname="">
+              <a class="vc_general vc_btn3  vc_btn3-shape-rounded  vc_btn3-color-success" style={{ backgroundColor: "#79b974" }} href="/Dashboard" >Access Here</a>
+            </div>
+
           </div>
-          <div id="PaneCenter" class="shadow p-3 mb-5 bg-body rounded col-sm-4">
-            <h5 style={{ textAlign: "center" }}>MyIR Mobile</h5>
+          <div id="PaneCenter" class="shadow p-3 mb-5 bg-body rounded col-sm-4" style={{ width: 300, textAlign: "center" }}>
+            <h2 style={{ fontSize: "h5", textAlign: "center" }}>MyIR Mobile</h2>
             <img src="/imgs/myirmobillogo.png" alt="MyIrMobile Logo" />
             <p>If you are looking for your household’s state COVID-19 Certificate</p>
             <p style={{ textAlign: "left", paddingTop: 30 }}>&nbsp;&nbsp;&nbsp;°&nbsp;Only in English</p>
 
             <div>
-              <a class="vc_general vc_btn3  vc_btn3-shape-rounded  vc_btn3-color-success" style={{ backgroundColor: "#79b974" }} href="https://app.myirmobile.com/auth/sign-in?state=WA" title="Click here to go to the MyIr Mobile site for Washington State" target="MyIrWA">Register Here</a>
+              <a class="vc_general vc_btn3  vc_btn3-shape-rounded  vc_btn3-color-success" style={{ backgroundColor: "#79b974" }} href="https://app.myirmobile.com/auth/sign-in?state=WA" target="MyIrWA">Register Here</a>
             </div>
           </div>
-          <div id="PaneRight" class="shadow p-3 mb-5 bg-body rounded col-sm-4">
-            <h5 style={{ textAlign: "center" }}>State School Certificate of Immunizations</h5>
+          <div id="PaneRight" class="shadow p-3 mb-5 bg-body rounded col-sm-4" style={{ width: 300, textAlign: "center" }}>
+            <h2 style={{ fontSize: "h5", textAlign: "center" }}>State School Certificate of Immunizations</h2>
             <img src="/imgs/wastateschoolcertimmunizationslogo.png" alt="WA State School Certification of Immunizations Logo" />
             <p>If you are looking for your children’s State School Certificate of Immunizations</p>
             <p style={{ textAlign: "left", paddingTop: 30 }}>&nbsp;&nbsp;&nbsp;°&nbsp;Only in English</p>
             <div>
-              <a class="vc_general vc_btn3  vc_btn3-shape-rounded  vc_btn3-color-success" style={{ backgroundColor: "#79b974" }} href="https://wa.myir.net/login/" title="Click here to go to the State School Certificate of Immunizations site" target="StateSchool">Register Here</a>
+              <a class="vc_general vc_btn3  vc_btn3-shape-rounded  vc_btn3-color-success" style={{ backgroundColor: "#79b974" }} href="https://wa.myir.net/login/" target="StateSchool">Register Here</a>
             </div>
           </div>
         </div>
