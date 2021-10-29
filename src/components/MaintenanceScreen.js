a805a8703525836b8c07c8dd81b03c1e01141632
George Elliott <George.Elliott@doh.wa.gov>
2021-10-28 20:39:35 -0700

    Styling on the Maintenance page

--- src/components/MaintenanceScreen.js
+++ src/components/MaintenanceScreen.js
@@ -1,19 +1,21 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 
 const Maintenance = () => {
   return (
     <div class="maintainance-container">
     <div class="fluid-container">
         <div class="maintainance-image">
-            <div class="vaccineLogo">
-                  <img style={{ alignSelf: "left", paddingRight: 15 }} alt={"Wa State Seal"} width="120px" src="/imgs/doh_logo_doh-black.png" />
-                  <span class='logoDescription' style="font-size: 18px; display: inline-block; padding-left: 17px; color: #22489c; vertical-align: super; font-weight: 700; line-height: 1.3em; text-align: left;">Digital COVID-19 <br /> Vaccine Record</span>
-              </div>
+        <div className='vaccineLogo' style={{ padding: '5px 1.5vw' }}>
+            <Link to='/' style={{ display: 'inline-block', height: 'inherit', margin: '0', width: 'inherit' }}>
+              <img style={{ alignSelf: "center" }} alt={"WaVerify Logo"} width="200px" src="/imgs/waverifylogo.png" />
+              <span className='logoDescription' style={{ fontSize: '18px', display: 'inline-block', paddingLeft: '17px', color: '#22489c' }}>Digital COVID-19 Vaccine Record</span>
+            </Link>
+          </div>
         </div>
         <div class="maintainance-content">
             <div class="maintainance-article">
-                <h1 style="color: rgb(240, 103, 36); font-size:2.6em">We'll be back soon</h1>
+                <h1 style={{color: rgb(240, 103, 36), fontSizeAdjust:2.6}}>We'll be back soon</h1>
                 <p>Site is temporarily unavailable for maintenance. <br/> Please
                     check back soon. We appreciate your patience!</p>
             </div>
