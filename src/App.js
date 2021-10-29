e395e09ed53a54377c9ee0e68a8a0d9087842441
James Norris <James.Norris@dohmobile.onmicrosoft.com>
2021-10-28 20:00:22 -0700

    Adding maintenance page

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ function App() {
       <div className="App">
         <Router>
           <Header />
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/received" component={ReceivedScreen} />
-          <Route path="/FAQ" component={FAQScreen} />
+          <Route exact path="/maintenancepage.html" component={Dashboard} />
+          {/* <Route path="/received" component={ReceivedScreen} /> */}
+          {/* <Route path="/FAQ" component={FAQScreen} />
           <Route path="/SplashScreen" component={SplashScreen} />          
-          <Route path="/qr/:lang/:id" component={QrScreen} />
+          <Route path="/qr/:lang/:id" component={QrScreen} /> */}
         </Router>
         <Footer />
       </div>
