2258243f2e397c4ab7ed55d7b64c0f668c333bf2
George Elliott <George.Elliott@doh.wa.gov>
2021-11-15 17:14:52 -0800

    Added the 'import {TRANS} to FAQ Page

--- src/components/FAQScreen.js
+++ src/components/FAQScreen.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
 import ReactGA from "react-ga";
 import "../disclosureButton.js";
 import { Trans } from "react-i18next";
