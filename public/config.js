1716f5ca26af6e893ff3e027703356d86a60c63d
James Allen <James.Allen@doh.wa.gov>
2022-03-08 14:07:37 -0800

    AB#1163 AB#1872 Fixed languages not in date-fns to use English

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.config = {
     CREDENTIALS_API_STATUS: 'https://dev-waverify-api.doh.wa.gov',
     CREDENTIALS_API_QR: 'https://dev-waverify-api.doh.wa.gov',
-    CREDENTIALS_GA_DEPARTMENT: 'yourGoogleAnalyticsID'
+    CREDENTIALS_GA_DEPARTMENT: 'G-HHSVC4W7NS'
 };
