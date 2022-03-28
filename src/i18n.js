cbc17f971aaf321e588036caf8404ba32b0f817c
James Norris <James.Norris@dohmobile.onmicrosoft.com>
2022-03-28 12:31:59 -0700

    Testing

--- src/i18n.js
+++ src/i18n.js
@@ -4,6 +4,14 @@ import Backend from "i18next-http-backend";
 // This module will detect the language for us.
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const {APPLICATION_INSIGHTS_INSTRUMENTATION_KEY} = window.config;
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: { APPLICATION_INSIGHTS_INSTRUMENTATION_KEY }
+} });
+
+
 
 const customFallbacks = {
     'zh-MO': ['zh-TW'],
@@ -26,6 +34,9 @@ i18n
       // See: https://www.i18next.com/principles/fallback#fallback-to-different-languages
       load: "currentOnly",
       fallbackLng: (code) => {
+
+          appInsights.trackTrace({message: 'Requested Language Code: ' + code});
+          //test
           // Set English as default
           if (!code || code === 'en') return ['en'];
 
