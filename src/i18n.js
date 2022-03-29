6a8ea972def5c66a6da73e5881cc10ab6d770325
James Norris <James.Norris@dohmobile.onmicrosoft.com>
2022-03-29 09:35:28 -0700

    Revert "testing"
    This reverts commit 0de6637ad02278b35b216b80ee342b2c8c1a447e, reversing
    changes made to cbc17f971aaf321e588036caf8404ba32b0f817c.

--- src/i18n.js
+++ src/i18n.js
@@ -34,6 +34,7 @@ i18n
       // See: https://www.i18next.com/principles/fallback#fallback-to-different-languages
       load: "currentOnly",
       fallbackLng: (code) => {
+
           appInsights.trackTrace({message: 'Requested Language Code: ' + code});
           //test
           // Set English as default
